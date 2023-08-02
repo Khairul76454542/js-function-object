2753eccbb749a29c50ba92e1dd21942ee190886d
khairul76454542 <khairul76454542@gmail.com>
2023-08-03 04:41:22 +0600

    added same new

--- parameter.js
+++ parameter.js
@@ -1,5 +1,23 @@
+// function bringSingara(money) {
+//  console.log("mama singara den");
+//  console.log(money);
+// }
+// bringSingara(100);
+
 function bringSingara(money) {
-  console.log("mama singara den");
   console.log(money);
+  console.log("ai nen singara");
 }
-bringSingara(100);
+var taka = 300;
+// bringSingara(250);
+
+function add(num1, num2) {
+  console.log("going to add:", num1, num2);
+}
+// add(125, 96);
+
+function sum(a, b, c, d, e) {
+  var sum = a + b + c + d + e;
+  console.log(sum);
+}
+sum();
