33d770cb3b9967db5ec5a69f9d42b50d99e3b719
khairul76454542 <khairul76454542@gmail.com>
2023-08-03 02:42:14 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,12 @@
+// var name = 'Ariana Grande';
+// function declaration
+function startFan() {
+  console.log("Stand Up");
+  console.log("walk towards the switch");
+  console.log("press the switch");
+}
+
+// call the funtion
+startFan();
+console.log();
+startFan();
