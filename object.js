8d4fbd2130db790c9ed46c386a1a33db9b78cab3
khairul76454542 <khairul76454542@gmail.com>
2023-08-04 07:04:01 +0600

    added same new

---
+++ object.js
@@ -0,0 +1,21 @@
+var student = {
+  id: 115,
+  name: "solamin Khan",
+  class: 9,
+  marks: 98,
+};
+var mobile = {
+  brand: "samsung",
+  price: 19000,
+  storage: "64gb",
+  camera: "7mp",
+};
+
+var myComputer = {
+  brand: "lonovo",
+  price: 39000,
+  color: "silver",
+  processor: "i7",
+};
+
+console.log(myComputer);
